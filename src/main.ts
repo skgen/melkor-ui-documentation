8ebae5eb8e9ff9ce8489560a241f45337dbc3859
Lucas Chapuis <l.chapuis@patriarche.fr>
2022-12-02 18:57:55 +0100

    fix: remove debug mode

--- src/main.ts
+++ src/main.ts
@@ -15,35 +15,7 @@ app.use(createPinia());
 app.use(createMelkorUi({
   router,
   i18n,
-  debug: true,
+  debug: false,
 }));
 
 app.mount('#app');
-
-// window.random = () => {
-//   let all: HTMLElement[] = [...document.querySelectorAll('*')];
-//   const forbid = ['mk-TheApp', 'pux-AppDocLayout', 'mk-AppTableRow'];
-//   all = all.filter((item) => {
-//     const cl = [...item.classList].join(' ');
-//     for (const f of forbid) {
-//       if (cl.indexOf(f) !== -1) {
-//         return false;
-//       }
-//     }
-//     return cl.indexOf('mk-') !== -1;
-//   });
-//   console.log(all);
-//   for (const item of all) {
-//     item.style.transition = 'transform 500ms';
-//     // item.style.display = 'block';
-//     item.addEventListener('mousemove', (e) => {
-//       const rect = item.getBoundingClientRect();
-//       item.style.transform = `translate(${e.clientX - rect.left}px, ${e.clientY - rect.top}px)`;
-//     });
-//   }
-//   console.log(all.length);
-// };
-
-// setTimeout(() => {
-//   window.random();
-// }, 2000);
