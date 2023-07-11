a19e37da4862ea5c0cf1e5a2316c624e5b004205
Lucas Chapuis <l.chapuis@patriarche.fr>
2023-07-11 17:42:15 +0200

    feat: update doc for v1.20.2

---
+++ src/components/changelogs/TheV1.20.2.vue
@@ -0,0 +1,18 @@
+<template>
+  <AppChangelogVersion stable>
+    v1.20.2
+  </AppChangelogVersion>
+  <h3>Minor</h3>
+  <div>
+    <AppChangelogCommit sha="0cfa07cb0b4ad09e9074afcbacf4cba98512cb71">
+      <p>
+        Updated <code>Time</code> <code>private</code> properties to <code>public</code>
+      </p>
+    </AppChangelogCommit>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import AppChangelogVersion from '@/components/AppChangelogVersion.vue';
+import AppChangelogCommit from '@/components/AppChangelogCommit.vue';
+</script>
