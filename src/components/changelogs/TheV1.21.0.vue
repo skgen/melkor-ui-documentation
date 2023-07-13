0c1b937064a16248728e06c9d07ef7b5fc058b0f
Lucas Chapuis <l.chapuis@patriarche.fr>
2023-07-13 11:55:44 +0200

    feat: update doc for v1.21.0

---
+++ src/components/changelogs/TheV1.21.0.vue
@@ -0,0 +1,18 @@
+<template>
+  <AppChangelogVersion stable>
+    v1.21.0
+  </AppChangelogVersion>
+  <h3>New components</h3>
+  <div>
+    <AppChangelogCommit sha="6c9059b81dd5fa39543d81c3ffe5cae21018f539">
+      <p>
+        Menu Group
+      </p>
+    </AppChangelogCommit>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import AppChangelogVersion from '@/components/AppChangelogVersion.vue';
+import AppChangelogCommit from '@/components/AppChangelogCommit.vue';
+</script>
