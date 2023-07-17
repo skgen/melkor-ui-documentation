f24c56b1f421800d7873f4f44141e32a06497b0b
Lucas Chapuis <l.chapuis@patriarche.fr>
2023-07-17 15:46:54 +0200

    feat: update doc for v1.22.0

---
+++ src/components/changelogs/TheV1.22.0.vue
@@ -0,0 +1,21 @@
+<template>
+  <AppChangelogVersion stable>
+    v1.22.0
+  </AppChangelogVersion>
+  <h3>New components</h3>
+  <div>
+    <AppChangelogCommit sha="ac394fc6d2f88017c652ec3ccd68b438fdf44bf5">
+      <p>
+        Input File Preview
+      </p>
+      <p>
+        Input Textable Cancel
+      </p>
+    </AppChangelogCommit>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import AppChangelogVersion from '@/components/AppChangelogVersion.vue';
+import AppChangelogCommit from '@/components/AppChangelogCommit.vue';
+</script>
