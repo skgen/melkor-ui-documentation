91064c2e56e3eed07b37e80f526f4dc05746bddf
Lucas Chapuis <l.chapuis@patriarche.fr>
2023-07-19 16:13:58 +0200

    chore: clean main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-
 import { createMelkorUi } from '@patriarche/melkor';
+
 import '@patriarche/melkor/style';
 import '@/assets/scss/main.scss';
 
 import i18n from '@/plugins/i18n';
 import router from '@/plugins/router';
-
 import App from '@/App.vue';
-import '@style/main.scss';
 
 const app = createApp(App);
 
