97e2c1bedea227ea07af90fc1c322940f48d3ee4
Lucas Chapuis <l.chapuis@patriarche.fr>
2023-08-23 17:05:15 +0200

    chore: update i18n plugin config

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,22 +1,26 @@
 import { createI18n } from 'vue-i18n';
-import { getPersistedLocale } from '@patriarche/melkor';
 import merge from 'lodash/merge';
 
+import { getPersistedLocale } from '@patriarche/melkor';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import melkorEn from '@patriarche/melkor/i18n/en';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import melkorFr from '@patriarche/melkor/i18n/fr';
-import en from '@i18n/en.json';
 import fr from '@i18n/fr.json';
-
-const docFr = merge({}, en, fr);
+import en from '@i18n/en.json';
 
 const messages = {
   en: merge({}, melkorEn, en),
-  fr: merge({}, melkorFr, docFr),
+  fr: merge({}, melkorFr, fr),
 };
+
+const locales = Object.keys(messages);
 
 const i18n = createI18n({
   legacy: false,
-  locale: getPersistedLocale(Object.keys(messages)) ?? import.meta.env.VITE_APP_DEFAULT_LOCALE,
+  locale: getPersistedLocale(locales) ?? import.meta.env.VITE_APP_DEFAULT_LANG,
   messages,
 });
 
