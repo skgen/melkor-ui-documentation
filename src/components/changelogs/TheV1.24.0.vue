7fd758946663eb5c07e32b0dff547fa1cc9af374
Lucas Chapuis <l.chapuis@patriarche.fr>
2023-09-04 17:45:10 +0200

    build: update doc for v1.24.0

---
+++ src/components/changelogs/TheV1.24.0.vue
@@ -0,0 +1,22 @@
+<template>
+  <AppChangelogVersion stable>
+    v1.24.0
+  </AppChangelogVersion>
+  <h3>Enhancements</h3>
+  <div>
+    <AppChangelogBreakingChange>
+      <AppChangelogCommit sha="54ba91cf4f9dafc79339623ef1e58c3343fa012e">
+        Hyphenated custom <code>emit</code> from `useInput` hook to improve generic type inference
+      </AppChangelogCommit>
+    </AppChangelogBreakingChange>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import AppChangelogVersion from '@/components/AppChangelogVersion.vue';
+import AppChangelogCommit from '@/components/AppChangelogCommit.vue';
+import AppChangelogBreakingChange from '@/components/AppChangelogBreakingChange.vue';
+// import AppChangelogWarningChange from '@/components/AppChangelogWarningChange.vue';
+
+// const appName = import.meta.env.VITE_APP_NAME;
+</script>
